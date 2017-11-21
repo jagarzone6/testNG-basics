ae5fed2e1a5e725da1b92311456c865c565ca51f
jorgeg <jorgeg@blanclabs.com>
2017-11-21 14:02:35 -0500

    pom file

---
+++ demo/test/examples/Annotations.java
@@ -0,0 +1,12 @@
+package examples;
+import org.testng.annotations.Test;
+
+public class Annotations {
+
+
+    @Test
+            public void OpeningBrowser() {
+        System.out.println("TESTNG");
+    }
+
+    }
