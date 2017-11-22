f9ed000abc5d000d6601b46cf3ec7e5c109dbd03
jorgeg <jorgeg@blanclabs.com>
2017-11-22 09:20:04 -0500

    annotations

--- demo/test/examples/Annotations.java
+++ demo/test/examples/Annotations.java
@@ -1,12 +1,37 @@
 package examples;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class Annotations {
 
+    @BeforeTest
+    public void Before() {
+        System.out.println("Before all");
+    }
+
+    @BeforeMethod
+    public void BeforeTest() {
+        System.out.println("Before test");
+    }
 
     @Test
-            public void OpeningBrowser() {
-        System.out.println("TESTNG");
+    public void Btest() {
+        System.out.println("B TESTNG");
     }
 
+
+    @Test
+    public void Atest() {
+        System.out.println("A TESTNG");
     }
+
+
+    @AfterTest
+    public void AfterAll(){
+        System.out.println("END");
+
+    }
+}
